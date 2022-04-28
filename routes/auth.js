b2a4e8a5ddd5e20c71e297521c37b127ed12764b
Kepoulpe <moreno.n@hotmail.fr>
2022-04-28 20:20:27 +0200

    create .env and changes on routes

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,6 @@ const authCtrl = require('../controllers/auth');
 // route for user can signup
 router.post('/signup', authCtrl.authSignup);
     
-
 // route for user can login
 router.post('/login', authCtrl.authLogin);
     
