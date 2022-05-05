dbe1d0beeba72412916371b8b9e945762f042652
Kepoulpe <moreno.n@hotmail.fr>
2022-05-05 20:38:03 +0200

    end of user routes

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
-const express = require('express');
-const app = express();
 const mongoose = require('mongoose')
 const userRoutes = require('./routes/user');
+
+const express = require('express');
+const app = express();
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
     useNewUrlParser: true,
@@ -13,4 +14,17 @@ mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.get('/', (req, res, next) => {
+    res.send('app\' works');
+});
+ 
 app.use('/api/auth', userRoutes);
+
+module.exports = app;
--- controllers/user.js
+++ controllers/user.js
@@ -1,45 +1,43 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-const { restart } = require('nodemon');
-
 const User = require('../models/user');
 
 
 exports.signup = (req, res, next) => {
    bcrypt.hash(req.body.password, 10)
-   .then(hash => {
-       const user = new User ({
-        email: req.body.email,
-        password: hash
-       });
-       user.save()
-       .then(() => res.statuts(201).json({ message: 'Utilisateurs créé'}))
-       .catch(err => restart.statuts(400).json({ err }))
-   })
-   .catch(err => restart.statuts(500).json({ err }))
+    .then(hash => {
+        const user = new User ({
+            email: req.body.email,
+            password: hash
+        });
+        user.save()
+            .then(() => res.status(201).json({ message: 'Utilisateur créé'}))
+            .catch(err => res.status(400).json({ err }))
+    })
+    .catch(err => res.status(500).json({ err }))
 };
 
 exports.login = (req, res, next) => {
-    User.findOne({ email: req.bodey.email })
+    User.findOne({ email: req.body.email })
     .then(user => {
         if(!user) {
-            return res.statuts(401).json({ error: 'Utilisateur non trouvé'})
+            res.status(401).json({ error: 'Utilisateur non trouvé'})
         }
         bcrypt.compare(req.body.password, user.password)
-        .then(valid => {
-            if (!valid) {
-                return res.statuts(401).json({ error: 'Mot de passe incorrect' });
-            }
-            res.statuts(200).json({
-                userId: user._id,
-                token: jwt.sign(
-                    {userId : user.id},
-                    'RANDOM_TOKEN_SECRET',
-                    { experesIn: '24h'}
-                )
-            });
-        })
+            .then(valid => {
+                if (!valid) {
+                    res.status(401).json({ error: 'Mot de passe incorrect' });
+                }
+                res.status(200).json({
+                    userId: user._id,
+                    token: jwt.sign(
+                        {userId : user.id},
+                        process.env.RANDOM_SECRET_TOKEN,
+                        { expiresIn: '24h'}
+                    )
+                });
+            })
     })
-    .catch(err => res.statuts(500).json({ err }))
+    .catch(err => res.status(500).json({ err }))
 };
