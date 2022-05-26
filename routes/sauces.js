7365af9206a97fb650a8cce229624c423ee97216
Kepoulpe <moreno.n@hotmail.fr>
2022-05-26 20:15:19 +0200

    add multer package and controllers

--- routes/sauces.js
+++ routes/sauces.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // user can create one thing in the data base mongoDB
-router.post('/api/stuff', (req, res, next) => {
+router.post('/api/sauces', (req, res, next) => {
     delete req.body._id;
     const thing = new Thing({
         ...req.body
@@ -13,28 +13,28 @@ router.post('/api/stuff', (req, res, next) => {
 });
 
 // modify one specific thing in the database mongoDB
-router.put('/api/stuff/:id', (req, res, next) => {
+router.put('/api/sauces/:id', (req, res, next) => {
     Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
         .then(() => res.status(200).json({ message: 'Objet modifié !' }))
         .catch(error => res.status(400).json({ error }));
 });
 
 // delete one thing on the data base mongoDB
-router.delete('/api/stuff/:id', (req, res, next) => {
+router.delete('/api/sauces/:id', (req, res, next) => {
     Thing.deleteOne({ _id: req.params.id })
         .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
         .catch(error => res.status(400).json({ error }));
 });
 
 // get one thing in the data base mongoDB 
-router.get('/api/stuff/:id', (req, res, next) => {
+router.get('/api/sauces/:id', (req, res, next) => {
     Thing.findOne({ _id: req.params.id })
         .then(thing => res.status(200).json(thing))
         .catch(error => res.status(404).json({ error }));
 });
 
 // get all the things in the data base mongoDB
-router.get('/api/stuff', (req, res, next) => {
+router.get('/api/sauces', (req, res, next) => {
     Thing.find()
         .then(things => res.status(200).json(things))
         .catch(error => res.status(400).json({ error }));
