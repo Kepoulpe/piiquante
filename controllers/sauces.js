4ba742e6c384419657fcbd40919e2cf0838eb47b
Kepoulpe <moreno.n@hotmail.fr>
2022-06-21 21:39:46 +0200

    crate test dor like/dislike logic

--- controllers/sauces.js
+++ controllers/sauces.js
@@ -2,6 +2,7 @@ const Thing = require('../models/sauces');
 const fs = require('fs');
 const { markAsUntransferable } = require('worker_threads');
 const { error, Console } = require('console');
+const { likeDislikeSauceLogic } = require('./likeDislikeLogic');
 
 // user can create one sauce in the data base mongoDB
 exports.createThing = (req, res, next) => {
@@ -58,108 +59,124 @@ exports.getAllThings = (req, res, next) => {
         .catch(error => res.status(400).json({ error }));
 };
 
-
-// Unique function to test in sauces.test.js
-// exports.validateLikeDislikeSaucePayload = (payload) => {
-//     const {userId} = payload;
-//     if (userId == '' || typeof userId !== 'string') {
-//         return { error: 'Veuillez vous identifier' };
-//     }
-// }
-
 // post like/dislike for one sauce
-exports.likeDislikeSauce = (req, res, next) => {
-    const sauceId = req.params.id
-    const userId = req.body.userId
-    const like = req.body.like
+exports.likeDislikeSauce = async (req, res, next) => {
+
+    let sauceRecord;
+
+    // 1) we check if the thing to update exists
+    try {
+        sauceRecord = await Thing.findOne({_id: req.params.id});
+    } catch (error) {
+        res.status(404).json({ error: "Sauce non trouvée !" });
+        return;
+    }
+
+    // 2) we create a formatted object from the sauce obtained in MongoDB from previous step
+    const {userId, likes, dislikes, usersLiked, usersDisliked} = sauceRecord;
+    sauceRecord._id = req.params.id;
+
+    // 3) we get the payload that we will use to actually update like/dislike data with MongoDB
+    const updateLikeDislikeObj = likeDislikeSauceLogic(sauceRecord, req.body)
+
+    // 3bis) send appropriate response if updateLikeDislikeObj is false (user tried to update likes data on his own sauce)
+
+    // 4) we update the sauce data in MongoDB
+    // TODO pass to Thing.updateOne the formatted update object => {$inc: { likes: 1 }, $push: { usersLiked: userId }, _id: sauceId}
+
+    // 5) we send a response to the user
+
+    // const sauceId = req.params.id
+    // const userId = req.body.userId
+    // const like = req.body.like
 
     // update sauce object in db with new like/dislike OR without previous like/dislike
-    switch (like) {
-        // if like = 1, add userId to usersLiked array field in database
-        case 1:
-            Thing.updateOne({ _id: sauceId }, {
-                likes: +1,
-                usersLiked: userId ,
-                _id: sauceId
-            })
-            //     // return success message
-                .then(() => {
-                    res.status(201).json({ message: 'Like enregistré' });
-                    console.log('Like sauce updated')
-                })
-                // return error message
-                .catch((error) => {
-                    res.status(400).json({ error: error });
-                    console.log('Like not added')
-                })
-            break;
+    // switch (like) {
+    //     // if like = 1, add userId to usersLiked array field in database
+    //     case 1:
+    //         Thing.updateOne({ _id: sauceId }, {
+    //             $inc: { likes: 1 },
+    //             $push: { usersLiked: userId },
+    //             _id: sauceId
+    //         })
+    //         //     // return success message
+    //             .then(() => {
+    //                 res.status(201).json({ message: 'Like enregistré' });
+    //                 console.log('Like sauce updated')
+    //             })
+    //             // return error message
+    //             .catch((error) => {
+    //                 res.status(400).json({ error: error });
+    //                 console.log('Like not added')
+    //             })
+    //         break;
 
-        // if like = -1, add userId to usersDisliked array field in database
-        case -1:
-            Thing.updateOne({ _id: sauceId }, {
-                dislikes: +1 ,
-                usersDisliked: userId,
-                _id: sauceId
-            })
-                // return success message
-                .then(() => {
-                    res.status(201).json({ message: ' Dislike enregistré' });
-                    console.log('Dislike sauce updated')
-                })
-                // return error message
-                .catch((error) => {
-                    res.status(400).json({ error: error });
-                    console.log('Dislike not added')
-                })
-            break;
+    //     // if like = -1, add userId to usersDisliked array field in database
+    //     case -1:
+    //         Thing.updateOne({ _id: sauceId }, {
+    //             $inc: { dislikes: 1 },
+    //             $push: { usersDisliked: userId },
+    //             _id: sauceId
+    //         })
+    //             // return success message
+    //             .then(() => {
+    //                 res.status(201).json({ message: ' Dislike enregistré' });
+    //                 console.log('Dislike sauce updated')
+    //             })
+    //             // return error message
+    //             .catch((error) => {
+    //                 res.status(400).json({ error: error });
+    //                 console.log('Dislike not added')
+    //             })
+    //         break;
 
-        // if like = 0, remove userId to usersLiked/usersDisliked array field in database
-        case 0:
-            Thing.findOne({sauceId})
-                .then((sauce) => {
-                    // user unlike
-                    if (sauce.usersLiked.find(user => user === userId)) {
-                        Thing.updateOne({ _id: sauceId }, {
-                            likes: -1 ,
-                            $pull: { usersLiked: userId },
-                            _id: sauceId
-                        })
-                            // return success message
-                            .then(() => {
-                                res.status(201).json({ message: ' Like retiré' });
-                                console.log('Like removed')
-                            })
-                            // return error message
-                            .catch((error) => {
-                                res.status(400).json({ error: error });
-                                console.log('Like not removed')
-                            })
-                        //user undislike
-                    } if (sauce.usersDisliked.find(user => user === userId)) {
-                        Thing.updateOne({ _id: sauceId }, {
-                            dislikes: -1 ,
-                            $pull: { usersDisliked: userId },
-                            _id: sauceId
-                        })
-                            // return success message
-                            .then(() => {
-                                res.status(201).json({ message: 'Dislike retiré' });
-                                console.log('Dislike removed')
-                            })
-                            // return error message
-                            .catch((error) => {
-                                res.status(400).json({ error: error });
-                            })
-                    }
-                })
-                // sauce doesnt exist or not found
-                .catch((error) => {
-                    res.status(404).json({ error: error });
-                    console.log('Sauce not found')
-                    console.log(error)
-                })
-                .break;
-        default:
-            console.error('Something went wrong')
-    }
+    //     // if like = 0, remove userId to usersLiked/usersDisliked array field in database
+    //     case 0:
+    //         Thing.findOne({sauceId})
+    //             .then((sauce) => {
+    //                 // user unlike
+    //                 if (sauce.usersLiked.find(user => user === userId)) {
+    //                     Thing.updateOne({ _id: sauceId }, {
+    //                         $inc: { likes: -1 },
+    //                         $pull: { usersLiked: userId },
+    //                         _id: sauceId
+    //                     })
+    //                         // return success message
+    //                         .then(() => {
+    //                             res.status(201).json({ message: ' Like retiré' });
+    //                             console.log('Like removed')
+    //                         })
+    //                         // return error message
+    //                         .catch((error) => {
+    //                             res.status(400).json({ error: error });
+    //                             console.log('Like not removed')
+    //                         })
+                        
+    //                 } else if (sauce.usersDisliked.find(user => user === userId)) { //user undislike
+    //                     Thing.updateOne({ _id: sauceId }, { 
+    //                         $inc: { dislikes: -1 },
+    //                         $pull: { usersDisliked: userId },
+    //                         _id: sauceId
+    //                     })
+    //                         // return success message
+    //                         .then(() => {
+    //                             res.status(201).json({ message: 'Dislike retiré' });
+    //                             console.log('Dislike removed')
+    //                         })
+    //                         // return error message
+    //                         .catch((error) => {
+    //                             res.status(400).json({ error: error });
+    //                         })
+    //                 }
+    //             })
+    //             // sauce doesnt exist or not found
+    //             .catch((error) => {
+    //                 res.status(404).json({ error: error });
+    //                 console.log('Sauce not found')
+    //                 console.log(error)
+    //             })
+    //             break; 
+    //     default:
+    //         console.error('Something went wrong')
+    // }
 }
--- controllers/sauces.test.js
+++ controllers/sauces.test.js
@@ -1,35 +1,98 @@
-const saucesCtlr = require("./sauces");
-const validateLikeDislikeSaucePayload = saucesCtlr.validateLikeDislikeSaucePayload;
+const sauceLogic = require("./likeDislikeLogic");
+const likeDislikeSauceLogic = sauceLogic.likeDislikeSauceLogic;
 
 // test suite
-describe("testing validating like/dislike payload", () => {
+describe("testing like/dislike", () => {
 
-    it("should return the correct error payload when sending an empty user id", () => {
+    it("given a user who has created a sauce, when he wants to update like/dislike data, then he cant", () => {
         // arrange
-        const expected = { error: 'Veuillez vous identifier' };
-        const testPayload = {
-            sauceId: 1,
-            userId: '',
-            like: true
+        const expected = false;
+        const testInitialSauce = {
+            _id: "1234",
+            userId: "x", 
+            likes: 0, 
+            dislikes: 0, 
+            usersLiked: [], 
+            usersDisliked: [] 
         };
+        const testReqPayload = {
+            like: 1,
+            userId: "x"
+        }
         // act
-        const actual = validateLikeDislikeSaucePayload(testPayload);
+        const actual = likeDislikeSauceLogic(testInitialSauce, testReqPayload);
+        // assert
+        expect(actual).toBeFalsy();
+    });
+
+    it("given a user who has NOT created a sauce, when he likes a sauce, then the likes prop of the sauce is incremented by 1", () => {
+        // arrange
+        const expected = {$inc: { likes: 1 }, $push: { usersLiked: ["x"] }, _id: "1234"};
+        const testInitialSauce = {
+            _id: "1234",
+            userId: "y", 
+            likes: 0, 
+            dislikes: 0, 
+            usersLiked: [], 
+            usersDisliked: [] 
+        };
+        const testReqPayload = {
+            like: 1,
+            userId: "x"
+        }
+        // act
+        const actual = likeDislikeSauceLogic(testInitialSauce, testReqPayload);
         // assert
         expect(actual).toEqual(expected);
     });
 
-    it("should return the correct error payload when sending a number for a user id", () => {
+    it("given a user who has NOT created a sauce, when he likes a sauce, then his userId should be pushed to the usersLiked array", () => {
         // arrange
-        const expected = { error: 'Veuillez vous identifier' };
-        const testPayload = {
-            sauceId: 1,
-            userId: 1,
-            like: true
+        const expected = {$inc: { likes: 1 }, $push: { usersLiked: ["x"] }, _id: "1234"};
+        const testInitialSauce = {
+            _id: "1234",
+            userId: "y", 
+            likes: 0, 
+            dislikes: 0, 
+            usersLiked: [], 
+            usersDisliked: [] 
         };
+        const testReqPayload = {
+            like: 1,
+            userId: "x"
+        }
+        // // act
+        const actual = likeDislikeSauceLogic(testInitialSauce, testReqPayload);
+        // // assert
+        expect(actual).toEqual(expected);
+    });
+
+    it("given a user who has NOT created a sauce, when he likes a sauce that he has already liked, then it should return false", () => {
+        // arrange
+        const expected = false;
+        const testInitialSauce = {
+            _id: "1234",
+            userId: "y", 
+            likes: 0, 
+            dislikes: 0, 
+            usersLiked: ["x"], 
+            usersDisliked: [] 
+        };
+        const testReqPayload = {
+            like: 1,
+            userId: "x"
+        }
         // act
-        const actual = validateLikeDislikeSaucePayload(testPayload);
+        const actual = likeDislikeSauceLogic(testInitialSauce, testReqPayload);
         // assert
         expect(actual).toEqual(expected);
     });
 
 });
+
+
+// userId: '6298d87392fc5f24573a1c86',
+// likes: 0,
+// dislikes: -1,
+// usersLiked: [],
+// usersDisliked: [],
